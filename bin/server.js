9190365898d4dd907c7d67475de14d5804e9ca74
Vladimir Kriukov <zeoniss28@gmail.com>
2021-06-29 22:28:36 +0300

    summary

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
-const app = require('../app')
+const app = require("../app")
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 3005
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`)
